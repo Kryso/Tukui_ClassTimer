ba611e48d064e18ae57f8a761a35070ea864fb6a
Lukas Novotny <asyrks@gmail.com>
2010-02-18 15:11:22 +0100

    fixed hyperspeed accelerators id added some spell ids

--- core.lua
+++ core.lua
@@ -104,31 +104,36 @@ local SORT_DIRECTION = true;
 
 -- Trinket filter - mostly for trinket procs, delete or wrap into comment block --[[  ]] if you dont want to track those
 local TRINKET_FILTER = {
-		CreateSpellEntry( 71432 ), -- Mote of Anger
-		CreateSpellEntry( 72412 ), -- Frostforged Champion
-		CreateSpellEntry( 67671 ), -- Banner of Victory
-		CreateSpellEntry( 67703 ), CreateSpellEntry( 67708 ), CreateSpellEntry( 67772 ), CreateSpellEntry( 67773 ), -- Death Choice
-		CreateSpellEntry( 60229 ), -- Greatness (Strength)
-		CreateSpellEntry( 60233 ), -- Greatness (Agility)
-		CreateSpellEntry( 60234 ), -- Greatness (Intellect)
-		CreateSpellEntry( 60235 ), -- Greatness (Spirit)
-		CreateSpellEntry( 71485 ), CreateSpellEntry( 71556 ), -- Deathbringer's Will - Agility of the Vrykul
-		CreateSpellEntry( 71492 ), CreateSpellEntry( 71560 ), -- Deathbringer's Will - Speed of the Vrykul		
-		CreateSpellEntry( 71487 ), CreateSpellEntry( 71557 ), -- Deathbringer's Will - Precision of the Iron Dwarves
-		CreateSpellEntry( 71491 ), CreateSpellEntry( 71559 ), -- Deathbringer's Will - Aim of the Iron Dwarves
-		CreateSpellEntry( 71486 ), CreateSpellEntry( 71558 ), -- Deathbringer's Will - Power of the Taunka
-		CreateSpellEntry( 71484 ), CreateSpellEntry( 71561 ), -- Deathbringer's Will - Strength of the Taunka		
-		CreateSpellEntry( 71402 ), -- Whispering Fang Skull
-		CreateSpellEntry( 67684 ), -- Hospitality / Talisman of Resurgence
-		CreateSpellEntry( 71570 ), -- Cultivated Power / Muradin's Spyglass
+		CreateSpellEntry( 67671 ), -- Fury(Banner of Victory)
+
+		CreateSpellEntry( 60229 ), -- Greatness (Greatness - Strength)
+		CreateSpellEntry( 60233 ), -- Greatness (Greatness - Agility)
+		CreateSpellEntry( 60234 ), -- Greatness (Greatness - Intellect)
+		CreateSpellEntry( 60235 ), -- Greatness (Greatness - Spirit)
+		
+		CreateSpellEntry( 67703 ), CreateSpellEntry( 67708 ), CreateSpellEntry( 67772 ), CreateSpellEntry( 67773 ), -- Paragon (Death Choice)
+		CreateSpellEntry( 67684 ), -- Hospitality (Talisman of Resurgence)
+		
+		CreateSpellEntry( 71432 ), -- Mote of Anger (Tiny Abomination in a Jar)	
+		CreateSpellEntry( 71485 ), CreateSpellEntry( 71556 ), -- Agility of the Vrykul (Deathbringer's Will)
+		CreateSpellEntry( 71492 ), CreateSpellEntry( 71560 ), -- Speed of the Vrykul (Deathbringer's Will)
+		CreateSpellEntry( 71487 ), CreateSpellEntry( 71557 ), -- Precision of the Iron Dwarves (Deathbringer's Will)
+		CreateSpellEntry( 71491 ), CreateSpellEntry( 71559 ), -- Aim of the Iron Dwarves (Deathbringer's Will)
+		CreateSpellEntry( 71486 ), CreateSpellEntry( 71558 ), -- Power of the Taunka (Deathbringer's Will)
+		CreateSpellEntry( 71484 ), CreateSpellEntry( 71561 ), -- Strength of the Taunka (Deathbringer's Will)
+		CreateSpellEntry( 71570 ), CreateSpellEntry( 71572 ), -- Cultivated Power (Muradin's Spyglass)
 		CreateSpellEntry( 71605 ), CreateSpellEntry( 71636 ), -- Phylactery of the Nameless Lich
+		CreateSpellEntry( 71401 ), CreateSpellEntry( 71541 ), -- Icy Rage (Whispering Fanged Skull)
+		CreateSpellEntry( 71396 ), -- Herkuml War Token
+		CreateSpellEntry( 72412 ), -- Frostforged Champion (Ashen Band of Unmatched/Endless Might/Vengeance)
+
 		CreateSpellEntry( 59626 ), -- Black Magic
-		CreateSpellEntry( 45758 ), -- Hyperspeed Acceleration
+		CreateSpellEntry( 54758 ), -- Hyperspeed Acceleration (Hyperspeed Accelerators)
 		CreateSpellEntry( 55637 ), -- Lightweave
 		
-		CreateSpellEntry( 2825, true ), -- Bloodlust
-		CreateSpellEntry( 32182, true ), -- Heroism
-		CreateSpellEntry( 26297 ), -- Troll Racial
+		CreateSpellEntry( 2825, true ), CreateSpellEntry( 32182, true ), -- Bloodlust/Heroism
+		CreateSpellEntry( 26297 ), -- Berserking (troll racial)
+		CreateSpellEntry( 33702 ), CreateSpellEntry( 33697 ), CreateSpellEntry( 20572 ), -- Blood Fury (orc racial)
 	};
 	
 --[[ Class specific filters
@@ -180,6 +185,7 @@ local CLASS_FILTERS = {
 				CreateSpellEntry( 57623 ), -- Horn of Winter
 			},
 			procs = {
+				CreateSpellEntry( 49016 ), -- Hysteria
 				CreateSpellEntry( 53365 ), -- Unholy Strength	
 				CreateSpellEntry( 71227 ), -- Sigil of the Hanged Man				
 			}
